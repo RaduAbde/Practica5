1968700e8bfb087bde901a9785b4177a1c40ea4a
RaduAbde <raduabde@gmail.com>
2024-01-08 17:01:18 +0100

    Parte 7 Punto 6

--- app/src/main/java/ui/SettingsFragment.kt
+++ app/src/main/java/ui/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import net.iessochoa.radwaneabdessamie.practica5.R
 
@@ -8,5 +11,15 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        val buildVersion: Preference? = findPreference("buildVersion")
+//definimos la accion para la preferencia
+        buildVersion?.setOnPreferenceClickListener {
+            startActivity(
+                Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://portal.edu.gva.es/03013224/"))
+            )
+            //hay que devolver booleano para indicar si se acepta el cambio o  no
+            false
+        }
     }
 }
